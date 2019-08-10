13708b9bd9d8c327fcec5f16ccd13a35e2d3d2f2
Ivan Grigorov <ivangrigorov9@gmail.com>
2019-08-10 23:40:42 +0300

    Adding First Tests

---
+++ tests/AutoLoader/AutoLoaderTest.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once __DIR__."\..\..\DI\AutoLoader\AutoLoader.php";
+include_once __DIR__."\..\..\DI\Errors\WorkflowErrors.php";
+
+use PHPUnit\Framework\TestCase;
+use WorkflowErrors;
+
+
+class AutoLoaderTest extends TestCase {
+
+    public function test_AutoLoader_load_shouldReturnFalseWithInvalidPath(): void
+    {
+        // Arrange
+        $autoloader = new AutoLoader();
+        $fileToUpload = "invalidPath";
+
+        // Act and Assert
+
+        $autoloader->load($fileToUpload);
+        $this->assertFalse(in_array($fileToUpload, get_included_files()));
+
+    }
+
+    public function test_AutoLoader_load_shouldReturnTrueWithValidPath(): void
+    {
+        // Arrange
+        $autoloader = new AutoLoader();
+        $fileToUpload = __DIR__."\..\..\DI\Lib\Config.php";
+
+        // Act and Assert
+
+        $autoloader->load($fileToUpload);
+        var_dump(get_included_files());
+        $this->assertTrue(in_array(realpath($fileToUpload), get_included_files()));
+
+    }
+}
---
+++ tests/Utils/UtilsTest.php
@@ -0,0 +1,52 @@
+<?php
+
+include_once __DIR__."\..\..\DI\Utils\Utils.php";
+include_once __DIR__."\..\..\DI\Errors\GlobalExceptions.php";
+include_once __DIR__."\..\..\DI\Errors\WorkflowErrors.php";
+
+use PHPUnit\Framework\TestCase;
+use GlobalExcpetions\ParameterNotGIvenException;
+use WorkflowErrors\ConvertingInterfaceToClassNameException;
+
+
+class UtilsTest extends TestCase {
+
+    public function test_Utils_extractClassNameFromInterfaceName_shouldThrowExceptionWhenParamNotGiven(): void
+    {
+        // Arrange
+        $utils = new Utils();
+
+        // Act and Assert
+
+        $this->expectException(ArgumentCountError::class);
+        $utils->extractClassNameFromInterfaceName();
+
+    }
+
+    public function test_Utils_extractClassNameFromInterfaceName_shouldThrowExceptionWhenWrongInterfaceGiven(): void
+    {
+        // Arrange
+        $utils = new Utils();
+        $interface = "Test";
+
+        // Act and Assert
+
+        $this->expectException(ConvertingInterfaceToClassNameException::class);
+        $utils->extractClassNameFromInterfaceName($interface);
+
+    }
+
+    public function test_Utils_extractClassNameFromInterfaceName_shouldReturnCorrectClassName(): void
+    {
+        // Arrange
+        $utils = new Utils();
+        $interface = "ITest";
+
+        // Act and Assert
+
+        $className = $utils->extractClassNameFromInterfaceName($interface);
+        $this->assertEquals("_Test", $className);
+    }
+
+    
+}
